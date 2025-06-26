6d5868eeb376c98ff46e7f15c6f2b1232b587732
giresh03 <135005276+giresh03@users.noreply.github.com>
2025-06-27 00:01:32 +0530

    Delete frontend directory

--- frontend/calculate/expirationTime.js
+++
@@ -1,6 +0,0 @@
-const expirationTime = () => {
-  return new Date().getTime() + 60 * 60 * 1000;
-  // adding an hour
-};
-
-export default expirationTime;
--- frontend/src/utils.js
+++
@@ -1,6 +0,0 @@
-// const BASE_URL = "http://localhost:4000";
-
-// const BASE_URL = "https://weeebs.onrender.com"
-const BASE_URL = "https://weeebs-nkm3.onrender.com"
-
-export default BASE_URL;
